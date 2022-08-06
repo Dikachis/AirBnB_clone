79b34faef14fc33cbcce108155a5289812ee03e0
Dikachis <dikachi015safety@gmail.com>
2022-08-06 22:47:26 +0100

    Fix & Updated base_models.py file

--- models/base_model.py
+++ models/base_model.py
@@ -15,21 +15,21 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if kwargs is not None:
+        if kwargs:
             dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
-                if key == 'created_at':
-                    value = datetime.strptime(value, dtime_format)
-                if key == 'updated_at':
-                    value = datetime.strptime(value, dtime_format)
-                setattr(self, key, value)
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(kwargs['created_at'], dtime_format)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(kwargs['updated_at'], dtime_format)
+                else:
+                    setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)  # as instructed also in task 5 for new instance
 
     def __str__(self):
@@ -37,7 +37,7 @@ class BaseModel:
         of BaseModel instances"""
 
         clsName = self.__class__.__name__
-        return "[%s] (%s) %s" % (clsName, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at
