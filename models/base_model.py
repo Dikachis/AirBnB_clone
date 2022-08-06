6b396beb47b35cb29dc11ff705e7e64fef0a01d8
Dikachis <dikachi015safety@gmail.com>
2022-08-07 00:29:02 +0100

    Fix & Updated base_models.py file

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,10 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         if kwargs:
             dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
             for key, value in kwargs.items():
