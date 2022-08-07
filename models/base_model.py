a2e6a67c7f70bd844f97154fed3fe67b529fdfa0
Dikachis <dikachi015safety@gmail.com>
2022-08-07 13:38:39 +0100

    Fixed $ Added some files

--- models/base_model.py
+++ models/base_model.py
@@ -9,44 +9,26 @@ import models
 class BaseModel:
     """Base class for all models"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initialization of a Base instance.
         Args:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-        if kwargs:
-            dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at':
-                    self.created_at = datetime.strptime(
-                        kwargs['created_at'], dtime_format)
-                elif key == 'updated_at':
-                    self.updated_at = datetime.strptime(
-                        kwargs['updated_at'], dtime_format)
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)  # as instructed also in task 5
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a readable string representation
         of BaseModel instances"""
-
         clsName = self.__class__.__name__
         return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at
         with the current datetime"""
-
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary that contains all
