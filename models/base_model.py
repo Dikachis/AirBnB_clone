da4eddf91068bca4c5e93a84874a96c560cc314f
Dikachis <dikachi015safety@gmail.com>
2022-08-07 13:41:43 +0100

    Fixed $ Added some files

--- models/base_model.py
+++ models/base_model.py
@@ -15,20 +15,38 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        kwargs['created_at'], dtime_format)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        kwargs['updated_at'], dtime_format)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)  # as instructed also in task 5
 
     def __str__(self):
         """Returns a readable string representation
         of BaseModel instances"""
+
         clsName = self.__class__.__name__
         return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at
         with the current datetime"""
+
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary that contains all
@@ -40,7 +58,7 @@ class BaseModel:
         # my_dict['created_at'] = self.created_at.isoformat()
         # my_dict['__class__'] = clsName
         # OR
-        my_dict = dict()  # OR {}
+        my_dict = dict()
         my_dict['__class__'] = self.__class__.__name__
         for key, value in self.__dict__.items():
             if key in ('created_at', 'updated_at'):
