101d30a67f7f4e2b5e54f4cb61e967808fd3e760
Dikachis <dikachi015safety@gmail.com>
2022-08-08 01:00:40 +0100

    Added some codes

--- console.py
+++ console.py
@@ -9,17 +9,21 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)  '
 
     def do_EOF(self, line):
+        """Exits console"""
         print("")
         return True
 
     def do_quit(self, line):
+        """Quit command to exit the program"""
         print("Good Bye!")
         return True
 
     def help_quit(self):
+        """when two arguments involve"""
         print('\n'.join(["Quit command to exit the program"]))
 
     def emptyline(self):
+        """ overwriting the emptyline method """
         return False
         # OR
         # pass
