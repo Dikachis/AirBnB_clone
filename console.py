5cf456e9655ecfe40d20b7910363b471242a7483
Dikachis <dikachi015safety@gmail.com>
2022-08-11 23:14:08 +0100

    Added my console.py file interpreter

--- console.py
+++ console.py
@@ -7,12 +7,12 @@ import models
 from models.base_model import BaseModel
 from models import storage
 import json
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+# from models.user import User
+# from models.place import Place
+# from models.state import State
+# from models.city import City
+# from models.amenity import Amenity
+# from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
