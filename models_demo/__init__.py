e14e093a567046a50b63b48e031130e3ddc327a7
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-08-20 14:22:27 +0100

    Delete models_demo directory

--- models_demo/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""__init__ method for models package, or
-Module for FileStorage autoinit."""
-
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
--- models_demo/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Class Amenity """
-
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ Class Amenity """
-    name = ""
